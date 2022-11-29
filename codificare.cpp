d7399dec7ea84f9ee996551bc095a472553a5c4e
Auras <TheLimeYT14@gmail.com>
2022-11-29 11:44:14 +0200

    final tema

--- codificare.cpp
+++ codificare.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
 
 int main(){
+    ofstream out("cod.txt");
+    string a = {0}, b = {0};
+    int v = 0;
 
+    getline(cin, a);
+    cin >> v;
 
+    out << v;
+
+    for (int i = 0; i < a.length(); i++)
+    {
+        b[i] = (int(a[i]) + v) % 256;
+        out << b[i];
+    }         
+    
     return 0;
 }
--- decodificare.cpp
+++ decodificare.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main(){
-
-
-
+    ifstream fin("cod.txt");
+    string a = {0}, b = {0};
+    char firstchar[] = {0};
+    int v = 0;
+    
+    fin >> a;
+    
+    firstchar[0] = a[0];
+    v = stoi(firstchar);
+    
+    for (int i = 1; i < a.length(); i++)
+    {
+        b[i] = (int(a[i]) - v) % 256;
+        cout << b[i];
+    }
     
     return 0;
 }
